59861fa7bd155271b22f1e36808c51057bf89cf0
Simon Wendsche <simon.wendsche@gmx.de>
2025-04-25 20:26:47 +0200

    groundwork for gatling firing

--- script/RocketTurret.cs
+++ script/RocketTurret.cs
@@ -9,12 +9,15 @@ public partial class RocketTurret : Node3D
 	public Node3D CentralPillar;  // Only rotates around Y axis
 	[Export]
 	public Node3D RocketTubes;  // Only rotates around X axis
+	[Export]
+	public Node3D BulletPlane;
 
 	public override void _Ready() {
 	}
 
 	public override void _Process(double delta) {
 		AimAtTarget();
+		Fire();
 	}
 
 	// TODO limit rotation speed
@@ -22,7 +25,7 @@ public partial class RocketTurret : Node3D
 	private void AimAtTarget() {
 		if (Target == null)
 			return;
-		
+
 		var globalPos = CentralPillar.GlobalTransform.Origin;
 		var targetPos = Target.GlobalTransform.Origin;
 		var oldRotation = CentralPillar.GlobalTransform.Basis.GetRotationQuaternion();
@@ -35,5 +38,29 @@ public partial class RocketTurret : Node3D
 		var tubeRotation = new Quaternion(tubeTransform.Basis);
 		var tubeWRotation = RocketTubes.GlobalTransform.Basis.GetRotationQuaternion().Slerp(tubeRotation, 0.1f);
 		RocketTubes.GlobalBasis = new Basis(tubeWRotation);
+	}
+
+	private void Fire() {
+		if (BulletPlane == null)
+			return;
+
+		float maxDist = 200f;
+
+		var origin = RocketTubes.GlobalPosition;
+		var end = origin - RocketTubes.GlobalTransform.Basis.Z * maxDist;
+		var query = PhysicsRayQueryParameters3D.Create(origin, end);
+
+		var spaceState = GetWorld3D().DirectSpaceState;
+		var result = spaceState.IntersectRay(query);
+
+		float distance = maxDist;
+		if (result.Count > 0) {
+			var hitpoint = result["position"].AsVector3();
+			distance = (hitpoint - origin).Length();
+		}
+
+		BulletPlane.GlobalPosition = RocketTubes.GlobalPosition;
+		BulletPlane.GlobalRotation = RocketTubes.GlobalRotation;
+		BulletPlane.Scale = new Vector3(1, 1, distance);
 	}
 }
