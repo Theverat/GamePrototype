55e631808b9cfc65cee3108a6944404707723e86
Simon Wendsche <simon.wendsche@gmx.de>
2025-05-04 10:22:05 +0200

    improve turret script

--- script/turret.gd
+++ script/turret.gd
@@ -1,11 +1,11 @@
-@tool
+#@tool
 extends Node3D
 
 @export
 var center_pillar: Node3D = null
 var gun_barrel: Node3D = null
 
-var rotation_speed: float = 50
+var rotation_speed: float = 5.0
 var target: Vector3 = Vector3.ZERO
 var target_enemy: Node3D = null
 
@@ -18,9 +18,10 @@ func _process(delta: float) -> void:
 	rotate_center_pillar_to_target(delta)
 	
 	
-func choose_target() -> void:	
-	if target_enemy == null:
+func choose_target() -> void:
+	if not is_instance_valid(target_enemy):
 		# Target enemy dead, choose a new one
+		target_enemy = null
 		var enemies: Array[Node] = get_tree().get_nodes_in_group("enemy")
 		
 		var min_dist_sqr: float = 99999999.0
@@ -28,10 +29,15 @@ func choose_target() -> void:
 			var enemy: Node3D = enemy_node as Node3D
 			assert(enemy is Node3D)
 			
-			var dist_sqr: float = (enemy.global_position - global_position).length_squared()
-			if dist_sqr < min_dist_sqr:
-				min_dist_sqr = dist_sqr
-				target_enemy = enemy
+			if is_instance_valid(enemy) and enemy.is_inside_tree():
+				assert(get_tree() == enemy.get_tree())
+				var dist_sqr: float = (enemy.global_position - global_position).length_squared()
+				if dist_sqr < min_dist_sqr:
+					min_dist_sqr = dist_sqr
+					target_enemy = enemy
+		
+		#if target_enemy:
+			#print("Picked new enemy:", target_enemy.name)
 	
 	if target_enemy:
 		# Target enemy still alive, track it
@@ -40,14 +46,9 @@ func choose_target() -> void:
 
 func rotate_center_pillar_to_target(delta: float) -> void:
 	assert(center_pillar)
-
-	var curr_dir = -global_transform.basis.z
-	var curr_dir2d = Vector2(curr_dir.x, curr_dir.z).normalized()
-	var curr_angle = atan2(curr_dir2d.x, curr_dir2d.y)
 		
-	var dir = target - global_position
+	var dir = target - center_pillar.global_position
 	var dir2d = -Vector2(dir.x, dir.z).normalized()
 	var target_angle = atan2(dir2d.x, dir2d.y)
 	
-	#var target_angle = turret.global_position.angle_to_point(get_global_mouse_position()) + PI/2.0
 	center_pillar.global_rotation.y = rotate_toward(center_pillar.global_rotation.y, target_angle, rotation_speed * delta)
